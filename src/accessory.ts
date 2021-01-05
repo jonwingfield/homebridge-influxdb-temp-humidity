c96c193843d0c584b3046023f5156dd60b9740cc
Jon <wingfield.jon@gmail.com>
2021-01-05 00:11:47 -0500

    Add some logging.

--- src/accessory.ts
+++ src/accessory.ts
@@ -4,9 +4,11 @@ import {
     API,
     CharacteristicEventTypes,
     CharacteristicGetCallback,
+    Formats,
     HAP,
     Logging,
-    Service
+    Service,
+    Units
   } from "homebridge";
 import InfluxService from "./influxService";
 
@@ -14,7 +16,7 @@ let hap: HAP;
 
 export = (api: API) => {
     hap = api.hap;
-    api.registerAccessory('HomebridgeInflux', HomebridgeInflux);
+    api.registerAccessory('homebridge-influx', "HomebridgeInflux", HomebridgeInflux);
 }
 
 class HomebridgeInflux implements AccessoryPlugin {
@@ -29,18 +31,20 @@ class HomebridgeInflux implements AccessoryPlugin {
         this.log = log;
         this.name = config.name;
 
+        this.log("Initializing Influx");
+
         this.influxService = new InfluxService(config.influx);
 
         this.tempService = new hap.Service.TemperatureSensor(config.name);
         this.tempService
             .getCharacteristic(hap.Characteristic.CurrentTemperature)
-            .setProps({ minValue: -100, maxValue: 150 })
+            .setProps({ minValue: -100, maxValue: 150  })
             .on(CharacteristicEventTypes.GET, this.getTemperatureState.bind(this));
 
         this.humidityService = new hap.Service.HumiditySensor(config.name);
         this.humidityService
             .getCharacteristic(hap.Characteristic.CurrentRelativeHumidity)
-            .setProps({ minValue: 0, maxValue: 150 })
+            .setProps({ minValue: 0, maxValue: 100 })
             .on(CharacteristicEventTypes.GET, this.getHumidityState.bind(this));
         
         this.informationService = new hap.Service.AccessoryInformation()
@@ -59,15 +63,17 @@ class HomebridgeInflux implements AccessoryPlugin {
     private getRemoteState(service: 'Temperature' | 'Humidity', callback: CharacteristicGetCallback) {
         this.influxService.queryTempHumidity()
             .then(({ temp, humidity }) => {
-                temp = Math.round(temp * 10) / 10;
-                humidity = Math.round(humidity);
-                this.tempService.setCharacteristic(hap.Characteristic.CurrentTemperature, temp);
-                this.humidityService.setCharacteristic(hap.Characteristic.CurrentRelativeHumidity, humidity);
+                temp = (temp - 32) * 5 / 9;
+                const tempS = (Math.round(temp * 10) / 10).toString();
+                const humidityS = Math.round(humidity).toString();
+                this.log(`${tempS}C ${humidityS}% RH`);
+                this.tempService.setCharacteristic(hap.Characteristic.CurrentTemperature, tempS);
+                this.humidityService.setCharacteristic(hap.Characteristic.CurrentRelativeHumidity, humidityS);
                 switch (service) {
                     case 'Temperature':
-                        return callback(null, temp);
+                        return callback(null, tempS);
                     case 'Humidity':
-                        return callback(null, humidity);
+                        return callback(null, humidityS);
                 }
             }).catch(err => {
                 this.log(err);
